9d7c9dc658f16ecf200bb5f5532f0248acd87102
Jonathan Muñoz Morales <jonathan_tuenty@hotmail.com>
2024-03-14 23:20:04 +0100

    feat: added about us page

--- components/about-us/story.vue
+++ components/about-us/story.vue
@@ -1,8 +1,70 @@
 <template>
+  <main class="hk-about" :data-color="store.getTheme">
+    <UnderlineTitle class="hk-about__title" :color="'blue'">
+      {{ $t("about.title") }}
+    </UnderlineTitle>
+    <div class="hk-about__content">
+      <img class="hk-about__content-image" src="@/public/media/cover-page.png" alt="Cover page image" />
+      <h3>{{ $t("about.content-section.title") }}</h3>
+
+      <div class="hk-about__content-story">
+        <p>{{ $t("about.content-section.paragraph-1") }}</p>
+        <p>{{ $t("about.content-section.paragraph-2") }}</p>
+        <p>{{ $t("about.content-section.paragraph-3") }}</p>
+        <p>{{ $t("about.content-section.paragraph-4") }}</p>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script setup lang="ts">
+import { useHackacodeStore } from "~/stores/Hackacode";
+
+const store = useHackacodeStore();
 </script>
 
 <style lang="scss">
+.hk-about {
+  display: flex;
+  flex-direction: column;
+  height: fit-content;
+  padding-left: 10rem;
+  padding-right: 10rem;
+
+  &[data-color="light"] {
+    background-color: $font-color--light;
+    color: $font-color--dark;
+  }
+  &[data-color="dark"] {
+    background-color: $font-color--dark;
+    color: font-color--light;
+  }
+
+  &__content {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    h3 {
+      font-size: $font-size--big;
+    }
+  }
+
+  &__content-story {
+    padding-left: 10rem;
+    padding-right: 10rem;
+    padding-bottom: 10rem;
+
+    p {
+      font-size: $font-size--medium;
+      margin-top: 0;
+    }
+  }
+
+  &__content-image {
+    width: 80rem;
+    height: 30rem;
+  }
+}
 </style>
---
+++ components/underline-title.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="hk-underline">
+    <h2 :data-color="store.getTheme">
+      <slot></slot>
+      <div class="hk-underline__rectangle" :data-color="color"></div>
+    </h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useHackacodeStore } from "~/stores/Hackacode";
+
+defineProps({
+  color: {
+    type: String,
+    require: true
+  }
+})
+
+const store = useHackacodeStore();
+</script>
+
+<style lang="scss">
+.hk-underline {
+  display: block;
+  width: fit-content;
+  height: fit-content;
+
+  h2 {
+    font-size: $font-size--large;
+
+    &[data-color="light"] {
+      color: $font-color--dark;
+    }
+    &[data-color="dark"] {
+      color: $font-color--light;
+    }
+  }
+
+  &__rectangle {
+    width: 100%;
+    overflow: hidden;
+    height: 20px;
+    border-radius: 20px;
+    margin-top: -30px;
+
+    &[data-color="blue"] {
+      background-color: rgba($font-color--blue, 0.5);
+    }
+  }
+}
+</style>
