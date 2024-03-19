992d83a5f70a806b85aef9ed32a7d116bfd6eac9
Jonathan Muñoz Morales <jonathan_tuenty@hotmail.com>
2024-03-19 22:36:30 +0100

    feat: added log in page

---
+++ components/log-in/register.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="hk-register" :data-color="store.getTheme">
+    <div>
+      <h2>{{ $t('register.title') }}</h2>
+      <LogIn class="hk-register__fields" />
+    </div>
+    <img class="hk-register__img" src="@/public/media/login.png">
+  </main>
+</template>
+
+<script setup lang="ts">
+import { useHackacodeStore } from "~/stores/Hackacode";
+
+const store = useHackacodeStore();
+
+</script>
+
+<style lang="scss">
+.hk-register {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding-left: 17rem;
+  overflow: hidden;
+
+  &[data-color="light"] {
+    color: $font-color--dark;
+  }
+  &[data-color="dark"] {
+    color: $font-color--light;
+  }
+
+  h2 {
+    width: 35rem;
+    padding-left: 5rem;
+    font-size: $font-size--large;
+  }
+
+  &__img {
+    position: absolute;
+    transform: translateX(37rem) translateY(5.5rem);
+  }
+}
+</style>
